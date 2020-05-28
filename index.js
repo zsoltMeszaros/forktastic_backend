04e7c7768aa9c29a28027d30e1e8e4cbaa479d6e
zsoltMeszaros <meszazaolt13@gmail.com>
2020-05-28 12:33:54 +0200

    cors fix

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv/config')
 
 const app = express();
 
-
+// middlewares
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({extended: false}));
 
 app.use("/api/users", require("./routes/api/users"));
